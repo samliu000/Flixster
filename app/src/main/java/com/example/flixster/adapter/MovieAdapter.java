b242323c8e41d4da5c1d7d965a3103d85f90438b
samliu000 <44206874+samliu000@users.noreply.github.com>
2020-10-21 22:07:01 -0700

    Alternative Orientation Layouts Added

--- app/src/main/java/com/example/flixster/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapter/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adapter;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -65,7 +66,19 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageUrl;
+
+            // if landscape
+            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageUrl = movie.getBackDropPath();
+            }
+            else {
+                imageUrl = movie.getPosterPath();
+            }
+            // if portrait
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
+
         }
     }
 }
